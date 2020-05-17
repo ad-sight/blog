0967268593cf0e416e63e3d49b873655927f534b
Mehdi Karamnejad <me@mehdi.tech>
2020-05-16 20:54:22 -0700

    Moves the files back to the root folder

--- generate.sh
+++ generate.sh
@@ -1,25 +1,46 @@
 #!/bin/bash
 
+# Define urls and https
+from_url=http://localhost:2368
+to_url=adsight.ca/blog
+to_https=true
+
 # clean up
-ls | grep -v generate.sh | xargs rm -rf
+rm -rf static
 
 # Copy blog content
-wget --recursive --no-host-directories --directory-prefix=./static --adjust-extension --timeout=30 --no-parent --convert-links http://localhost:2368/
+wget --recursive --no-host-directories --directory-prefix=static --adjust-extension --timeout=30 --no-parent --convert-links ${from_url}/
 
 # Copy 404 page
-wget --no-host-directories --directory-prefix=./static --adjust-extension --timeout=30 --no-parent --convert-links --content-on-error --timestamping http://localhost:2368/404.html
+wget --no-host-directories --directory-prefix=static --adjust-extension --timeout=30 --no-parent --convert-links --content-on-error --timestamping ${from_url}/404.html
 
 # Copy sitemaps
-wget --recursive --no-host-directories --directory-prefix=./static --adjust-extension --timeout=30 --no-parent --convert-links http://localhost:2368/sitemap.xsl
-wget --recursive --no-host-directories --directory-prefix=./static --adjust-extension --timeout=30 --no-parent --convert-links http://localhost:2368/sitemap.xml
-wget --recursive --no-host-directories --directory-prefix=./static --adjust-extension --timeout=30 --no-parent --convert-links http://localhost:2368/sitemap-pages.xml
-wget --recursive --no-host-directories --directory-prefix=./static --adjust-extension --timeout=30 --no-parent --convert-links http://localhost:2368/sitemap-posts.xml
-wget --recursive --no-host-directories --directory-prefix=./static --adjust-extension --timeout=30 --no-parent --convert-links http://localhost:2368/sitemap-authors.xml
-wget --recursive --no-host-directories --directory-prefix=./static --adjust-extension --timeout=30 --no-parent --convert-links http://localhost:2368/sitemap-tags.xml
+wget --recursive --no-host-directories --directory-prefix=static --adjust-extension --timeout=30 --no-parent --convert-links ${from_url}/sitemap.xsl
+wget --recursive --no-host-directories --directory-prefix=static --adjust-extension --timeout=30 --no-parent --convert-links ${from_url}/sitemap.xml
+wget --recursive --no-host-directories --directory-prefix=static --adjust-extension --timeout=30 --no-parent --convert-links ${from_url}/sitemap-pages.xml
+wget --recursive --no-host-directories --directory-prefix=static --adjust-extension --timeout=30 --no-parent --convert-links ${from_url}/sitemap-posts.xml
+wget --recursive --no-host-directories --directory-prefix=static --adjust-extension --timeout=30 --no-parent --convert-links ${from_url}/sitemap-authors.xml
+wget --recursive --no-host-directories --directory-prefix=static --adjust-extension --timeout=30 --no-parent --convert-links ${from_url}/sitemap-tags.xml
 
-# Replace localhost with domain
-#LC_ALL=C find ./static -type f -not -wholename *.git* -exec sed -i '' -e 's/http:\/\/localhost:2368/https:\/\/adsight.ca\/blog/g' {} +
-#LC_ALL=C find ./static -type f -not -wholename *.git* -exec sed -i '' -e 's/localhost:2368/adsight.ca\/blog/g' {} +
-#LC_ALL=C find ./static -type f -not -wholename *.git* -exec sed -i '' -e 's/http:\/\/www.gravatar.com/https:\/\/www.gravatar.com/g' {} +
+# Replace localhost with real domain
+if [ "${to_https}" == true ];
+then LC_ALL=C find ./static -type f -not -wholename *.git* -exec sed -i '' -e "s,http://${from_url},https://${to_url},g" {} +;
+fi
+if [ "${to_https}" == false ];
+then LC_ALL=C find ./static -type f -not -wholename *.git* -exec sed -i '' -e "s,http://${from_url},http://${to_url},g" {} +;
+fi
+LC_ALL=C find ./static -type f -not -wholename *.git* -exec sed -i '' -e "s,${from_url},${to_url},g" {} +
+LC_ALL=C find ./static -type f -not -wholename *.git* -exec sed -i '' -e 's,http://www.gravatar.com,https://www.gravatar.com,g' {} +
+
+
+# Fix file extension issues
+LC_ALL=C find ./static -type f -not -wholename *.git* -exec sed -i '' -e "s,.pngng,.png,g" {} +
+LC_ALL=C find ./static -type f -not -wholename *.git* -exec sed -i '' -e "s,.pnggng,.png,g" {} +
+LC_ALL=C find ./static -type f -not -wholename *.git* -exec sed -i '' -e "s,.jpgjpg,.jpg,g" {} +
+LC_ALL=C find ./static -type f -not -wholename *.git* -exec sed -i '' -e "s,.jpjpg,.jpg,g" {} +
 
 # Set up Github Pages CNAME
+echo "${to_url}" > static/CNAME
+
+cp -R ./static/* ./
+rm -rf static
